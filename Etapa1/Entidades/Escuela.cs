0111c8d532db123b9c26047a9097bfbe567041de
= <=>
2019-11-08 15:11:04 -0300

    Programando

---
+++ Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,29 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+        public string Nombre{
+            get{ return nombre; }
+            set { nombre = value; }
+        }
+
+        public int AñoDeCreacion{get;set;}
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public TiposEscuela TipoEscuela { get; set; }
+
+        // public Escuela(string nombre, int año)
+        // {
+        //     this.nombre = nombre;
+        //     this.AñoDeCreacion = año;
+        // }
+
+        //Otra manera de hacer un constructor
+        public Escuela (string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre,año);
+        public override string ToString(){
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela}, \nPais: {Pais}, Ciudad: {Ciudad}";
+        }
+
+    }
+}
---
+++ Etapa1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string Nombre = "Natividad del Señor";
+            int AñoFundacion = 1992;
+
+
+            var escuela = new Escuela(Nombre,AñoFundacion);
+            escuela.Pais = "Argentina";
+            escuela.Ciudad = "Rosario";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            Console.WriteLine(escuela);
+        }
+    }
+}
